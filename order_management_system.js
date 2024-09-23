9d85b934faa6b8953194e8b708f280a0ad28a3fe
johnathanponsoldt <jponsoldt772@gmail.com>
2024-09-22 20:54:58 -0400

    Create placeOrder function

--- order_management_system.js
+++ order_management_system.js
@@ -19,3 +19,26 @@ order.push({
     ],
     status: "Pending"
 });
+
+// Task 3: Create a Function to Place an Order
+
+function placeOrder(customerName, ordereditems) {
+    for (let item of ordereditems) {
+        const product =inventory.find(p => p.name === item.name);
+      if (!product || product.quantity < item.quantity) {
+        console.error(`Insufficient stock for ${item.name}`);
+        return;
+      }
+    }
+    ordereditems.foreach(item => {
+        const product = inventory.find(p => p.name === item.name);
+        product.quantity -= item.quantity;
+    });
+    orders.push({
+        customerName: "Ashley Smith",
+        items: mouse, monitor
+        
+    });
+    status: "Pending"
+    console.log(`Order placed for $(customername)`);
+}
