c79d9a1db066e051670a2a31c9d21af18e20a8c3
johnathanponsoldt <jponsoldt772@gmail.com>
2024-09-22 20:11:24 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+
+   { name: 'Mouse', price: 45, quantity: 10 },
+   { name: 'Monitor', price: 250, quantity: 5 },
+   { name: 'Keyboard', price: 90, quantity: 6 },
+   { name: 'Controller', price: 60, quantity: 4 }   
+];
